cef596be64a401a119ec46df76348295a6aecb2e
Zeeshan Siddiqui <zeeshansd767@gmail.com>
2025-07-03 12:20:11 +0500

    commit changes

--- client/src/screens/Verify.jsx
+++ client/src/screens/Verify.jsx
@@ -21,23 +21,28 @@ const Verify = () => {
       }, 2000)
     } catch (err) {
       setError(err.response?.data?.message || 'Error verifying OTP');
-    
+
       setMessage('');
     }
   };
 
   return (
-    <div className='flex flex-col items-center justify-center p-10 text-white mt-22'>
-      <h3 className='text-3xl mb-5 text-white'>Verify Your Email</h3>
-      <form onSubmit={handleSubmit} className='bg-white p-8 rounded-lg text-black'>
-        <input type="email" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-3 mb-4 border rounded-md" required />
-        <input type="text" placeholder="Enter OTP" value={otp} onChange={(e) => setOtp(e.target.value)}
-          className="w-full p-3 mb-4 border rounded-md" required />
-        <button type="submit" className="bg-blue-600 text-white py-2 px-4 rounded">Verify</button>
-        {message && <p className="text-green-600 mt-4">{message}</p>}
-        {error && <p className="text-red-600 mt-4">{error}</p>}
-      </form>
+    <div className=''>
+      <div className='min-h-screen flex gap-2 flex-col items-center justify-center bg-gradient-to-r from-gray-900 via-black to-gray-900 px-4 text-white animate-slide-down'>
+        <form onSubmit={handleSubmit} className='w-full max-w-md bg-gray-800 p-8 rounded-lg shadow-2xl border border-gray-700'>
+          <h3 className='text-3xl font-bold text-center mb-5 text-white'>Verify Your Email</h3>
+          <div className='flex flex-col gap-4'>
+            <input type="email" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)}
+              className="px-4 py-2 rounded-md bg-gray-900 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+            <input type="text" placeholder="Enter OTP" value={otp} onChange={(e) => setOtp(e.target.value)}
+              className="px-4 py-2 rounded-md bg-gray-900 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500" required />
+            <button type="submit" className="bg-blue-600 text-white py-2 px-4 rounded">Verify</button>
+            {message && <p className="text-green-600 mt-4">{message}</p>}
+            {error && <p className="text-red-600 mt-4">{error}</p>}
+          </div>
+
+        </form>
+      </div>
     </div>
   );
 };
